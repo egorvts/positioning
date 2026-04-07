1744928d8762533c916712a4363aa28616386cc1
egorvts <egor.vetosh2310@gmail.com>
2026-04-07 19:16:42 +0500

    2

--- index.js
+++ index.js
@@ -5,3 +5,19 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+document.querySelector(".close-button").addEventListener("click", (event) => {
+    const element = document.querySelector(".lightbox");
+    element.style.display = "none";
+
+    const element1 = document.querySelector(".overlay");
+    element1.style.display = "none";
+});
+
+document.querySelector(".open-button").addEventListener("click", (event) => {
+    const element = document.querySelector(".lightbox");
+    element.style.display = "flex";
+
+    const element1 = document.querySelector(".overlay");
+    element1.style.display = "block";
+});
